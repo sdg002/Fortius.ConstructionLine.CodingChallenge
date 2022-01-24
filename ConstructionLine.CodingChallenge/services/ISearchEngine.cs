ec2bf5ac9467350834988cc29ebc350adba66f92
saurabh <saurabh_dasgupta@hotmail.com>
2022-01-24 21:52:23 +0000

    Moved classes into folders for entities and services

---
+++ ConstructionLine.CodingChallenge/services/ISearchEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge.services
+{
+    public interface ISearchEngine
+    {
+        SearchResults Search(SearchOptions options);
+    }
+}
--- ConstructionLine.CodingChallenge/services/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/services/SearchEngine.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using ConstructionLine.CodingChallenge.services;
+using System.Collections.Generic;
 
 namespace ConstructionLine.CodingChallenge
 {
-    public class SearchEngine
+    public class SearchEngine : ISearchEngine
     {
         private readonly List<Shirt> _shirts;
 
@@ -11,9 +12,7 @@ namespace ConstructionLine.CodingChallenge
             _shirts = shirts;
 
             // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
-
         }
-
 
         public SearchResults Search(SearchOptions options)
         {
